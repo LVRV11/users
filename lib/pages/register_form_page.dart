b029216c43cfeb103fb59345e4f8f7a3956d692f
LVRV11 <lavrovdmitrii@gmail.com>
2023-11-06 20:53:37 +0100

    first commit

---
+++ lib/pages/register_form_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class RegisterFormPage extends StatefulWidget {
+  const RegisterFormPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterFormPageState createState() => _RegisterFormPageState();
+}
+
+class _RegisterFormPageState extends State<RegisterFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register Form'),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Full name *'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Phone Number *'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Email Address'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Information'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Password *'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration:
+                  const InputDecoration(labelText: 'Confirm Passwpod *'),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Restart Quiz!',
+                style: TextStyle(color: Color.fromARGB(255, 176, 58, 162)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
